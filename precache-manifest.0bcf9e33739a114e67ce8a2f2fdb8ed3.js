9bc9ed433e029df9a57c1736ac9846c2279716a0
p-jaeeun <j_brick@naver.com>
2020-10-21 22:32:16 +0900

    Updates

---
+++ precache-manifest.0bcf9e33739a114e67ce8a2f2fdb8ed3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8ab9243789b1fc8d4fde36a2d27b61",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "30c6fd5c183ef62ad4fc",
+    "url": "/movie_app/static/css/main.fd42d6fe.chunk.css"
+  },
+  {
+    "revision": "25fd20164c507f35b04f",
+    "url": "/movie_app/static/js/2.b6b5f07a.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/movie_app/static/js/2.b6b5f07a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c6fd5c183ef62ad4fc",
+    "url": "/movie_app/static/js/main.9c5ded34.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
