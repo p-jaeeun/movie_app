66caa6e5dd53352293b12b4dfe88aaf04b24bcc6
p-jaeeun <j_brick@naver.com>
2020-10-23 19:01:10 +0900

    css updated

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ import "./Navigation.css";
 
 function Navigation() {
     return (
-        <div>
+        <div className="nav">
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
         </div>
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -10,7 +10,9 @@ class Detail extends React.Component{
     render() {
         const { location } = this.props;
         if (location.state) {
-            return <span>{location.state.title}</span>;      
+            return (
+                <span>{location.state.title}</span>
+            );      
         } else {
             return null;
         }
